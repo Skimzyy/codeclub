a090d53303b76bab60707c2c3155d22846aa36b4
Skimzyy <ogundimuroyan@yahoo.com>
2023-03-25 22:34:29 +0100

    Create bubblesort.py

---
+++ bubblesort.py
@@ -0,0 +1,14 @@
+#Bubble sort algorithm using python
+
+def bubbleSort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+
+arr = [2,4,10,5,3,1,7,6,8,9]
+newSort = bubbleSort(arr)
+print ("The sorted array is: ")
+for i in range(len(arr)):
+    print(arr[i])
